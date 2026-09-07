85660b3869ee2c23267addb8154339cb283aa43e
Okonprincewill <creditprincewill@gmail.com>
2026-09-07 13:02:37 +0100

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,7 +3,7 @@
 // GitHub: Okonprincewill/MATHSOCCER-4aSide-
 // ============================================================
 
-const CACHE_NAME = 'mathsoccer-v12';
+const CACHE_NAME = 'mathsoccer-v13';
 const APP_ROOT = '/MATHSOCCER-4aSide-/';
 
 // ✅ FILES TO CACHE - NO APP_ROOT ALONE!
